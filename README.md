724b0dd41e7e5c35948e04513593baa5eb572be2
Fabricio Veronez <fabricioveronez@gmail.com>
2024-02-01 09:26:23 -0300

    Suporte ao uso de arquivo de configuração

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # app-variaveis-ambiente
+
+Essa aplicação tem como objetivo ser um exemplo para aulas relacionadas ao uso e configuração de variáveis de ambiente.
+
+Variáveis de ambiente da aplicação:
+
+APP_NAME => Nome da aplicação
+APP_VERSION => Versão da aplicação
+APP_AUTHOR => Nome do autor
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -5,9 +6,9 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 // Definindo variáveis de ambiente com valores padrão
-const appName = process.env.APP_NAME || 'Aplicação Kubernetes';
-const appVersion = process.env.APP_VERSION || '1.0';
-const appAuthor = process.env.APP_AUTHOR || 'Desconhecido';
+const appName = process.env.APP_NAME;
+const appVersion = process.env.APP_VERSION;
+const appAuthor = process.env.APP_AUTHOR;
 
 app.get('/', (req, res) => {
     res.render("index", {appName, appVersion, appAuthor});
